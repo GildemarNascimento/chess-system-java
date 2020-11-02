1dc12b76d62ecc5d8790d2eccabb53aa2e91b710
GildemarNascimento <gildemargg@hotmail.com>
2020-11-02 15:49:48 -0300

    Chess layer and printing the board

---
+++ src/application/IU.java
@@ -0,0 +1,26 @@
+package application;
+
+import Chess.ChessPiece;
+
+public class IU {
+	
+	public static void printBoard(ChessPiece[][]pieces) {
+		for (int i=0; i<pieces.length; i++) {
+			System.out.print((8-i) + " ");
+			for(int j=0; j<pieces.length;j++) {
+				printPiece(pieces[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g");
+	}
+	private static void printPiece(ChessPiece peice) {
+		if (peice == null) {
+			System.out.print("-");
+		}else {
+			System.out.println(peice);
+		}
+		System.out.print(" ");
+	}
+
+}
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,13 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import Chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(3, 5);
-		
-		System.out.println(position);
-		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		IU.printBoard(chessMatch.getPieces());
 
 	}
 
