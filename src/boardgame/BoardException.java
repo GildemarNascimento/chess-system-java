2dbc9935b7e62fa60cd6f835ea03bdaca273d39f
GildemarNascimento <gildemargg@hotmail.com>
2020-11-04 19:36:19 -0300

    BoardExecption and defensive programing

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package boardgame;
+
+import boardgame.Board;
+public class BoardException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	public BoardException(String msg) {
+		super(msg);
+	
+	}
+}
