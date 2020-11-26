af8cbc45beea780d81d81ee8a3c936354468fa17
GildemarNascimento <gildemargg@hotmail.com>
2020-11-25 22:24:48 -0300

    Ignore commit folder

---
+++ src/Chess/ChessPiece.java
@@ -0,0 +1,38 @@
+package Chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+
+public abstract class ChessPiece extends Piece{
+	private Color color;
+	private int moveCount;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	public void increaseMoveCount() {
+		moveCount ++;
+	}
+	public void decreaseMoveCount() {
+		moveCount --;
+	}
+	protected boolean isThereOpponentPiece(Position position) {
+		//Verificar se a peça e do oponete.
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
+	
+}
